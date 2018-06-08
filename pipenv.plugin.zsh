1e05d897aefef7e1c3296a739266da70dc8405bc
Piotr Wasilewski <piotr.wasyl@gmail.com>
2018-06-08 10:27:32 +0200

    Update pipenv.plugin.zsh
    Spawning to pipenv shell

--- pipenv.plugin.zsh
+++ pipenv.plugin.zsh
@@ -7,3 +7,19 @@ if [[ "$(basename ${(%):-%x})" != "_pipenv" ]]; then
   autoload -U compinit && compinit
   compdef _pipenv pipenv
 fi
+
+
+# Called by zsh when directory changes, append to chpwd_functions
+chpwd_functions+=(chpwd_checkforpipenv)
+function chpwd_checkforpipenv() {
+    if [[ -a "${PWD}/Pipfile" ]]; then
+        if [[ "${VIRTUAL_ENV}" != $(pipenv --venv) ]]; then
+            echo -n "Pipfile exists. Do you want spawn a shell with the virtualenv? (y)"
+            read answer
+            if [[ "$answer" == "y" ]]; then
+                cat Pipfile | grep python_version
+                pipenv shell --fancy
+            fi
+        fi
+    fi
+}
